d8d50f1696dbc62028ede91faa6214c113d71a47
Keanu Budham <36482340+keanub23@users.noreply.github.com>
2022-12-05 21:10:31 -0500

    Update Server.py

--- backend/Server.py
+++ backend/Server.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 from TripToEmployment import fetch_trips_to_employment_map, fetch_employment_details
 from FetchRelationCount import get_count
+from COVIDcases import fetch_COVID_taxi
+from PaymentMethods import fetch_diff_payment_types
 from holidays import holiday
 import json
 
@@ -34,6 +36,19 @@ def hol():
     hol=holiday()
     # print(hol)
     return render_template("holidays.html")
+
+@app.route("/COVID", methods=['GET'])
+def fetch_COVID_taxi_controller():
+    trip_data = fetch_COVID_taxi()
+    #print(trip_data)
+    return render_template("COVID.html",trip_data=trip_data)
+
+@app.route("/payment", methods=['GET'])
+def fetch_diff_payment_types_controller():
+    print("HERE HERE HERE HERE HERE")
+    pay_data = fetch_diff_payment_types()
+    print('We got the pay data')
+    return render_template("payment.html",result=pay_data)
 
 if __name__ == '__main__':
     app.run(debug='true')
